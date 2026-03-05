9bfe510f3975c6b5f9ac00ef9921bf217a227ad1
Georgi <saukov.georgi@gmail.com>
2026-03-05 20:42:30 +0200

    Enhance hash_table function with removal confirmation
    Added a print statement to confirm removal of 'age' key.

--- Hash_tables_and_Basic_search.py
+++ Hash_tables_and_Basic_search.py
@@ -1,9 +1,6 @@
-#Hash tables and Basic search 
-#Hash table O(1) 
 def hash_table():
     hash_table = {}
     
-    # Adding key-value pairs to the hash table
     hash_table['name'] = 'Alice'
     hash_table['age'] = 30
     hash_table['city'] = 'New York'
@@ -13,13 +10,11 @@ def hash_table():
     print(hash_table['age'])   # Output: 30
     print(hash_table['city'])  # Output: New York
     
-    # Checking if a key exists in the hash table
     if 'name' in hash_table:
         print("Key 'name' exists in the hash table.")
     
-    # Removing a key-value pair from the hash table
     del hash_table['age']
     
-    # Checking if a key exists after deletion
     if 'age' not in hash_table:
+
         print("Key 'age' has been removed from the hash table.")
